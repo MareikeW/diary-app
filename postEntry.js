46570c01492b8162b042d68a0876ceaab7bffb94
49586574 <mareikewendler@web.de>
2020-07-15 16:57:39 +0200

    Klickevent: Neuer Tagebucheintrag wird unten angezeigt.

--- postEntry.js
+++ postEntry.js
@@ -1,5 +1,21 @@
-console.log("Hello!");
+var textField = document.getElementById("textArea"); // verknüpft Textfeld
 
-console.log(document.getElementsByTagName('h1'));
+var subButton = document.getElementById("btn"); // verknüpft Button
 
-console.log("Hello You!");
+// Wenn Button geklickt wird, wird die getTextContent()-Methode ausgeführt
+subButton.addEventListener("click", getTextContent);
+
+function getTextContent() {
+  // verhindert, dass Seite nach Klick neu geladen wird
+  event.preventDefault()
+  // neuer Absatz wird erstellt, hier soll der Text eingefügt werden
+  var newEntry = document.createElement("div");
+  // Absatz wird mit Text verknüpft
+  newEntry.textContent = textField.value;
+  // fügt class hinzu, zum Stylen
+  newEntry.className = "new-entry";
+  // neuer Absatz wird unter dem Button angezeigt
+  document.body.append(newEntry);
+  // Löscht den eingegebenen Text und Platzhalter wird wieder angezeigt
+  textField.value = "";
+}
