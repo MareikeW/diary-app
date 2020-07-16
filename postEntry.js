3007f296c9cb2c3f1c70a8e9cc6ea98f09d0461e
49586574 <mareikewendler@web.de>
2020-07-16 20:16:28 +0200

    Neue Farben + Problem mit required gelöst

--- postEntry.js
+++ postEntry.js
@@ -1,21 +1,22 @@
-var textField = document.getElementById("textArea"); // verknüpft Textfeld
+var entryForm = document.getElementById("formContainer");
+var textField = document.querySelector(".textArea"); // verknüpft Textfeld
+var entriesSection = document.querySelector("#entries"); // verknüpft mit Teil für Einträge
 
-var subButton = document.getElementById("btn"); // verknüpft Button
-
-// Wenn Button geklickt wird, wird die getTextContent()-Methode ausgeführt
-subButton.addEventListener("click", getTextContent);
-
-function getTextContent() {
+function getTextContent(event) {
   // verhindert, dass Seite nach Klick neu geladen wird
-  event.preventDefault()
+  event.preventDefault();
   // neuer Absatz wird erstellt, hier soll der Text eingefügt werden
   var newEntry = document.createElement("div");
-  // Absatz wird mit Text verknüpft
-  newEntry.textContent = textField.value;
   // fügt class hinzu, zum Stylen
   newEntry.className = "new-entry";
+  // Absatz wird mit Text verknüpft
+  newEntry.innerText = textField.value;
+  newEntry.style.color = "white";
   // neuer Absatz wird unter dem Button angezeigt
-  document.body.append(newEntry);
+  entriesSection.appendChild(newEntry);
   // Löscht den eingegebenen Text und Platzhalter wird wieder angezeigt
   textField.value = "";
 }
+
+// Wenn Button geklickt wird, wird die getTextContent()-Methode ausgeführt
+entryForm.addEventListener("submit", getTextContent);
